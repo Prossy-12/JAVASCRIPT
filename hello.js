df7f823d4238d2ac2e05c78cde3cd0e8c9a1c404
Prossy <Prossynalwoga047@gmail.com>
2024-07-02 11:30:12 +0300

    hello

---
+++ hello.js
@@ -0,0 +1,10 @@
+ //print hello world to the console
+ console.log("hello world")
+
+ //This my first comment in javascript
+ /*
+ this my
+  comment
+  l love my life
+  */
+console.log("hello","world")
